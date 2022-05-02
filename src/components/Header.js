57043a075982b33f4efc69d5a9b616d23fcea3a1
ericwafula <wathomeeric@gmail.com>
2022-05-02 16:53:37 +0300

    added tablet and mobile header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,53 @@
 import React from "react";
 import { useRef } from "react";
 import logo from "../assets/logo.png";
+import { HiMenuAlt3 } from "react-icons/hi";
 
 function Header() {
   return (
-    <div className="bg-color_Midnight_Express_DB pl-48 grid grid-cols-2 pt-4 pb-4 pr-48">
-      <div>
-        <img src={logo} alt="logo" />
+    <>
+      {/* desktop */}
+      <div className="hidden lg:block">
+        <div className="bg-color_Midnight_Express_DB pl-48 grid grid-cols-2 pt-4 pb-4 pr-48">
+          <div>
+            <img src={logo} alt="logo" />
+          </div>
+          <div className="flex items-center justify-end">
+            <ul className="flex flex-row justify-end gap-12 text-2xl">
+              <Li text="About" textColor="text-color_Governor_Bay_LLB" />
+              <Li text="Contact" textColor="text-color_Governor_Bay_LLB" />
+              <Li text="Login" textColor="text-white" />
+            </ul>
+          </div>
+        </div>
       </div>
-      <div className="flex items-center justify-end">
-        <ul className="flex flex-row justify-end gap-12 text-2xl">
-          <Li text="About" textColor="text-color_Governor_Bay_LLB" />
-          <Li text="Contact" textColor="text-color_Governor_Bay_LLB" />
-          <Li text="Login" textColor="text-white" />
-        </ul>
+      {/* tablet */}
+      <div className="hidden md:block lg:hidden">
+        <div className="bg-color_Midnight_Express_DB pl-12 grid grid-cols-2 pt-4 pb-4 pr-12">
+          <div className="w-12">
+            <img src={logo} alt="logo" />
+          </div>
+          <div className="flex items-center justify-end">
+            <ul className="flex flex-row justify-end gap-6 text-1xl">
+              <Li text="About" textColor="text-color_Governor_Bay_LLB" />
+              <Li text="Contact" textColor="text-color_Governor_Bay_LLB" />
+              <Li text="Login" textColor="text-white" />
+            </ul>
+          </div>
+        </div>
       </div>
-    </div>
+      {/* mobile */}
+      <div className="md:hidden">
+        <div className="bg-color_Midnight_Express_DB pl-4 grid grid-cols-2 pt-4 pb-4 pr-4">
+          <div className="w-12">
+            <img src={logo} alt="logo" />
+          </div>
+          <div className="flex items-center justify-end">
+            <HiMenuAlt3 size="2.5em" color="white"/>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
